22449fdebb794b02fb07ba7f55c1e518bbe91f30
David Pitman <david.pitman@cloudouble.com>
2020-11-15 07:46:53 +0000

    Ensure overwrite of existing objects in same namespace

--- core.js
+++ core.js
@@ -1,7 +1,7 @@
 /* globals */
 window.Cloudouble = {}
 window.Cloudouble.Core = {}
-window.Cloudouble.Core.Version = '1.0.1'
+window.Cloudouble.Core.Version = '1.0.2'
 window.Cloudouble.Core.Methods = {}
 window.Cloudouble.Core.Methods.getCookie = function(name) { var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)'); return v ? v[2] : null; }
 window.Cloudouble.Core.Methods.setCookie = function(name, value, _expires) { var d = new Date; d.setTime(_expires ? _expires*1000 : d.getTime()); document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString(); }
